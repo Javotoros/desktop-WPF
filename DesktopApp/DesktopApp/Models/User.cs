a3e9707750d27f4af37e911ce55da08f4797a08f
javtor719 <javtor719@gmail.com>
2026-01-19 18:30:42 +0100

    feat:Estructura inicial WFP

---
+++ DesktopApp/DesktopApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp.Models
+{
+    internal class User
+    {
+    }
+}
